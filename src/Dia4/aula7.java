ba18a8c781bbe9e608955c60fa616ad9a0dfbef0
João V. Santos <joaovaledosantos@gmail.com>
2024-02-22 14:14:37 -0300

    [Aula-37] Operador ternário

---
+++ src/Dia4/aula7.java
@@ -0,0 +1,9 @@
+package Dia4;
+
+public class aula7 {
+    public static void main(String[] args) {
+        int ingresso = 600;
+        String terAcesso = ingresso >=500 ? "VIP" : "Pista";
+        System.out.println(terAcesso);
+    }
+}
