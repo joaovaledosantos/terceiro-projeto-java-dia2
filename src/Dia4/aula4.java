8dd68200a2b26a3c32c915ad6107a10f6829ab2d
João V. Santos <joaovaledosantos@gmail.com>
2024-02-22 13:10:35 -0300

    [Aula-34] Operador lógico AND

---
+++ src/Dia4/aula4.java
@@ -0,0 +1,11 @@
+package Dia4;
+
+public class aula4 {
+    public static void main(String[] args) {
+        // priceProduct = 20
+        // Comprar somente entre $10 e $15
+        int priceProduct = 9;
+        boolean buy = priceProduct >=10 && priceProduct <=15;
+        System.out.println(buy);
+    }
+}
