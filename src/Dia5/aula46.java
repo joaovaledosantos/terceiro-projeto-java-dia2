b0a333ee4dbf3a5d7db09b181901fe9d96bc9377
João V. Santos <joaovaledosantos@gmail.com>
2024-02-23 08:25:15 -0300

    [Aula-46]  While Loops

---
+++ src/Dia5/aula46.java
@@ -0,0 +1,11 @@
+package Dia5;
+
+public class aula46 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i + " - Hi");
+            i++;
+        }
+    }
+}
