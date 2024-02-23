68732dbdd6a9d4b02594e459a05a348959120ae3
João V. Santos <joaovaledosantos@gmail.com>
2024-02-23 09:10:39 -0300

    [Aula-49] Dia 4 - Solução 2A

--- src/Dia5/aula48e49.java
+++ src/Dia5/aula48e49.java
@@ -3,7 +3,7 @@ package Dia5;
 public class aula48 {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++)
-            if (i % 2 == 0)
+            if (i % 2 != 0)
                 System.out.print(i + " ");
     }
 }
