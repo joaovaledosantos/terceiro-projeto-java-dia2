15b74d3ff243e740160bd0611669a2d8e96489ab
João V. Santos <joaovaledosantos@gmail.com>
2024-02-23 12:29:49 -0300

    [Aula-55]  Criando métodos

---
+++ src/Dia5/aula55.java
@@ -0,0 +1,11 @@
+package Dia5;
+
+public class aula55 {
+    public static void main(String[] args) {
+        productList("Laranja", 12);
+        productList("Banana", 17);
+    }
+    public static void productList(String productName, Integer productValue) {
+        System.out.println(productName + " - " + productValue);
+    }
+}
