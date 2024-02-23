53f981969d71d1606c7df55bc8472cd8f8b343b5
João V. Santos <joaovaledosantos@gmail.com>
2024-02-23 09:35:27 -0300

    [Aula-51] Dia 4 - Solução B

---
+++ src/Dia5/aula51.java
@@ -0,0 +1,10 @@
+package Dia5;
+
+public class aula51 {
+    public static void main(String[] args) {
+        String spaces = "Hello World";
+
+        for (int i = 0; i <= spaces.length() - 1; i++)
+        System.out.print(spaces.charAt(i) + " ");
+    }
+}
