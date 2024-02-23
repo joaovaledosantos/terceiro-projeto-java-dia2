b20bb2e713de4e50d398d488a03a265863c4af58
João V. Santos <joaovaledosantos@gmail.com>
2024-02-23 08:05:23 -0300

    [Aula-44] For Loops com Arrays

---
+++ src/Dia5/aula44.java
@@ -0,0 +1,10 @@
+package Dia5;
+
+public class aula44 {
+    public static void main(String[] args) {
+        String[] cars = {"BMW", "Tesla", "Tesla", "Tesla", "Ferrari"};
+
+        for (int i = 0; i < cars.length; i++)
+            System.out.print(cars[i] + " ");
+    }
+}
