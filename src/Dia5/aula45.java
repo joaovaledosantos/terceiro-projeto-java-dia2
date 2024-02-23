f35974e0761c1f5173ca430ec5462185340253f4
João V. Santos <joaovaledosantos@gmail.com>
2024-02-23 08:16:29 -0300

    [Aula-46] Nested Loops

---
+++ src/Dia5/aula45.java
@@ -0,0 +1,9 @@
+package Dia5;
+
+public class aula45 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 1; j++)
+                System.out.println(i + " " + j);
+     }
+}
