80319dedff56aae930ec53a17735ab3de6871dc1
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 07:02:02 -0300

    [Aula-63] Criando um Constructor

--- src/Dia6/aula62.java
+++ src/Dia6/aula62.java
@@ -3,10 +3,15 @@ package Dia6;
 public class aula62 {
         String firstName;
         int age;
+        int balance;
+
+        aula62() {
+            balance = 5;
+        }
 
         void newAccount() {
             System.out.println(firstName + " account Created!");
-            System.out.println("The new balance is $10");
+            System.out.println("The new balance is $" + balance);
         }
     }
 
