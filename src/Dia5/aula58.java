7c59ec4e649f4e060c326868cf2049e227c1f964
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 01:46:15 -0300

    [Aula-58] Extraindo a sua aplicação

---
+++ src/Dia5/aula58.java
@@ -0,0 +1,13 @@
+package Dia5;
+
+public class aula58 {
+    public static void main(String[] args) {
+        System.out.println("Lista os numeros pares de 0 a 20");
+        evenNumbers(20);
+    }
+    public static void evenNumbers(int numbers) {
+        for (int i = 0; i <= numbers; i++)
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+   }
+}
