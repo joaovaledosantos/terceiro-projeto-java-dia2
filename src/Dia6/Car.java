8f8c44327414cbc38ad0fb92dfcf671f7a50a7c0
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 08:54:19 -0300

    [Aula-68]  Dia 6 - Solução

--- src/Dia6/Car.java
+++ src/Dia6/Car.java
@@ -5,11 +5,17 @@ public class Car {
     private String marca;
     private String modelo;
     private int ano;
+    private int valor;
 
-    Car(String marca, String modelo,int ano) {
+    Car(String marca, String modelo,int ano, int valor) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
+        this.valor = valor;
+    }
+    public double valorVenda() {
+        double valorFinal = (valor * 0.1) + valor;
+        return valorFinal;
     }
 
     public String getMarca() {
--- src/Dia6/main.java
+++ src/Dia6/main.java
@@ -3,12 +3,13 @@ package Dia6;
 public class main {
     public static void main(String[] args) {
 
-        Car car = new Car("BMW", "X6", 2024);
+        Car car = new Car("BMW", "X6", 2024, 130_000);
 
         car.setModelo("X5");
 
         System.out.println(car.getMarca());
         System.out.println(car.getModelo());
         System.out.println(car.getAno());
+        System.out.println("Valor de venda: " + car.valorVenda());
     }
 }
