600a146b965b7851fe765a41f26e052105a5d52e
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 01:05:06 -0300

    [Aula-56] Corrigindo erros no codigo

---
+++ src/Dia5/aula56.java
@@ -0,0 +1,11 @@
+package Dia5;
+
+public class aula56 {
+    public static void main(String[] args) {
+        productList("Laranja", 12);
+        productList("Banana", 17);
+    }
+    public static void productList(String productName, Integer productValue) {
+        System.out.println(productName + " - " + productValue);
+    }
+}
