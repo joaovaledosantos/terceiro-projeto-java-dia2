be46bd94aeb10da9effd32fa780ff3af9d53cbfb
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 06:55:13 -0300

    [Aula-62]  Criando uma Classe

---
+++ src/Dia6/aula62.java
@@ -0,0 +1,13 @@
+package Dia6;
+
+public class aula62 {
+        String firstName;
+        int age;
+
+        void newAccount() {
+            System.out.println(firstName + " account Created!");
+            System.out.println("The new balance is $10");
+        }
+    }
+
+
---
+++ src/Dia6/main.java
@@ -0,0 +1,16 @@
+package Dia6;
+
+public class main {
+    public static void main(String[] args) {
+
+        aula62 c001 = new aula62();
+        c001.firstName = "Joao";
+        c001.age = 18;
+
+        aula62 c002 = new aula62();
+        c002.firstName = "Mariana";
+        c002.age = 35;
+
+        c002.newAccount();
+    }
+}
